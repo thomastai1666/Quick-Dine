c7bb972abc23fa961b27b443d030363b960e2f35
Thomas Tai <thomas.tai@rga.com>
2019-11-21 15:55:54 -0500

    Initial Commit

---
+++ Quick Dine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Dine
+//
+//  Created by Thomas Tai on 11/21/19.
+//  Copyright © 2019 Thomas Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
